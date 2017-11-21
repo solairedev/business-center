891fb6e8c6bf8e9342d7e11cc65f0ef71e7e80fd
solaire-off <victor374288@mail.ru>
2017-11-21 06:59:23 +0300

    Final blocks 'feedback' and 'offers'

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.sass')
 	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
-	//.pipe(autoprefixer(['last 15 versions']))
+	.pipe(autoprefixer(['last 15 versions']))
 	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream: true}));
